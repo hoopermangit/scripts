2b198a6931bc1b73de693f57a5bd1fb49d8cd294
hoopermangit <hooperman@hotmail.com>
2024-09-24 17:57:46 -0500

    Update 02.xfce.sh

--- 02.xfce.sh
+++ 02.xfce.sh
@@ -3,11 +3,11 @@
 # void linux remover xfce
 
 # remover lifgthdm
-sudo xbps-remove -R lightdm
+sudo xbps-remove -Ry lightdm
 
 # remover xfce
-sudo xbps-remove -R xfce4* xfwm4
-sudo xbps-remove -R libxfce4ui-4.18.6_1 xfce4-pulseaudio-plugin-0.4.8_1
+sudo xbps-remove -Ry xfce4* xfwm4
+sudo xbps-remove -Ry libxfce4ui-4.18.6_1 xfce4-pulseaudio-plugin-0.4.8_1
 sudo xbps-remove -Oov
 
 # iniciar el servicio sddm seleccionar plasma X11
