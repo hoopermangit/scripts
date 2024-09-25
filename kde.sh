9c797cf0c5cef9bd8840406c43e9dc2f9c47a29f
hoopermangit <hooperman@hotmail.com>
2024-09-25 08:47:27 -0500

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -30,3 +30,7 @@ sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/NetworkManager /var/service
 sudo ln -s /etc/sv/cupsd /var/service
 sudo ln -s /etc/sv/sddm /var/service
+
+# reiniciar sistema
+# seleccionar Plasma X11
+sudo reboot
