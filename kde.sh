f5b0519f0b635ad9acbdce161bd12e40747c269f
hoopermangit <hooperman@hotmail.com>
2024-09-27 06:23:00 -0500

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -10,7 +10,7 @@ sudo xbps-install -Suy
 
 # instalar paquetes
 
-sudo xbps-install -Sy alsa-pipewire ark barrier barrier-gui base-system bpytop brltty chromium chrony cryptsetup cups curl dejavu-fonts-ttf dialog epson-inkjet-printer-escpr espeakup fastfetch ffmpegthumbs filezilla font-misc-misc galculator gimp git gnome-keyring gnome-themes-standard gparted grub-i386-efi grub-x86_64-efi gutenprint gvfs-afc gvfs-mtp gvfs-smb gwenview hunspell-es  imagescan iscan kde5 kde5-baseapps kdenlive kolourpaint krita kwrite libreoffice libreoffice-i18n-es lvm2 mdadm mkvtoolnix-gui mpv nano network-manager-applet okular orca pipewire playonlinux qbittorrent rsync setxkbmap smplayer spectacle terminus-font udisks2 void-docs-browse void-live-audio wget wine wine-32bit xauth xmirror xorg-input-drivers xorg-minimal xorg-video-drivers xournal xtools-minimal
+sudo xbps-install -Sy alsa-pipewire ark barrier barrier-gui base-system bpytop brltty chromium chrony cryptsetup cups curl dejavu-fonts-ttf dialog epson-inkjet-printer-escpr espeakup fastfetch ffmpegthumbs filezilla font-misc-misc galculator gimp git gnome-keyring gnome-themes-standard gparted grub-i386-efi grub-x86_64-efi gutenprint gvfs-afc gvfs-mtp gvfs-smb gwenview hunspell-es  imagescan iscan kde5 kde5-baseapps kdenlive kolourpaint krita kwrite libreoffice libreoffice-i18n-es lvm2 mdadm mkvtoolnix-gui mpv nano network-manager-applet okular orca pipewire playonlinux qbittorrent rsync setxkbmap smplayer spectacle terminus-font udisks2 unrar void-docs-browse void-live-audio wget wine wine-32bit xauth xmirror xorg-input-drivers xorg-minimal xorg-video-drivers xournal xtools-minimal
 
 # instalar playonlinux
 sudo xbps-install -Sy playonlinux
