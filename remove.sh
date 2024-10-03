f6058064b71fd384dc2b051a6bed4500f920702a
hoopermangit <hooperman@hotmail.com>
2024-10-03 17:59:45 -0500

    Add files via upload

--- remove.sh
+++ remove.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# void linux remover xfce
+# remover firefox
+sudo xbps-remove -R firefox
+sudo xbps-remove -Oov
 
-# remover lifgthdm
-sudo xbps-remove -Ry lightdm
+# void linux remover xfce
 
 # remover xfce
 sudo xbps-remove -Ry xfce4* xfwm4
 sudo xbps-remove -Ry libxfce4ui-4.18.6_1 xfce4-pulseaudio-plugin-0.4.8_1
 sudo xbps-remove -Oov
+sudo xbps-install -Sy elogind
 
 # reiniciar sistema
 # seleccionar lxde
--- xfce.sh
+++ xfce.sh
@@ -3,8 +3,8 @@
 # void linux instalar xfce
 
 # remover firefox
-sudo xbps-remove -R firefox
-sudo xbps-remove -Oov
+# sudo xbps-remove -R firefox
+# sudo xbps-remove -Oov
 
 # actualizar repositorios
 sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree void-repo-debug
@@ -13,11 +13,14 @@ sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-no
 sudo xbps-install -Suy
 
 # instalar paquetes
-sudo xbps-install -Sy 7zip 7zip-unrar bpytop chromium epdfview epson-inkjet-printer-escpr fastfetch flameshot galculator gutenprint hunspell-es imagescan iscan kolourpaint libreoffice libreoffice-i18n-es mpv nano smplayer
+sudo xbps-install -Sy 7zip 7zip-unrar alsa-pipewire barrier barrier base-system bpytop brltty chromium chrony cryptsetup cups curl dejavu-fonts-ttf dialog epdfview epson-inkjet-printer-escpr espeakup fastfetch filezilla flameshot font-misc-misc galculator git gnome-keyring gnome-themes-standard gparted grub-i386-efi grub-x86_64-efi gutenprint gvfs-afc gvfs-mtp gvfs-smb hunspell-es imagescan iscan kolourpaint libreoffice libreoffice-i18n-es lightdm lightdm-gtk3-greeter lvm2 mdadm mousepad mpv nano network-manager-applet pipewire qbittorrent rsync setxkbmap smplayer terminus-font udisks2 void-docs-browse void-live-audio wget xauth xfce4 xfce4-pulseaudio-plugin xmirror xorg-input-drivers xorg-minimal xorg-video-drivers xtools-minimal
 
 # instalar fonts
 sudo cp -r Menlo.Font/ Windows.Fonts/ JetBrainsMono.Font/ /usr/share/fonts/
 sudo fc-cache -f -v
+
+# imagenes terminal y escritorio
+sudo cp -r Bodhi/ Magna/ /usr/share/backgrounds/xfce/
 
 # reiniciar sistema
 sudo reboot
