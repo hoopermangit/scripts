cbd45f885af099b1db7d84628010e4f56a7d0ddb
hoopermangit <hooperman@hotmail.com>
2024-10-06 09:13:54 -0500

    Update bodhi.sh

--- bodhi.sh
+++ bodhi.sh
@@ -7,13 +7,10 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 
 # instalar paquetes
-
 sudo apt-get install fastfetch hunspell-es libreoffice libreoffice-l10n-es lxde
 
 # instalar fonts
-
 sudo cp -r JetBrainsMono.Font/ Menlo.Font/ Office.Fonts/ Windows.Fonts/ /usr/share/fonts/
-
 sudo fc-cache -f -v
 
 # reiniciar sistema
