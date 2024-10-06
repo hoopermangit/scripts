ae568ee4263bcf3b58d30ae9347b2070f01acfd8
hoopermangit <hooperman@hotmail.com>
2024-10-06 08:34:32 -0500

    Delete services.sh

--- services.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# iniciar servicios
-sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/NetworkManager /var/service
-sudo ln -s /etc/sv/cupsd /var/service
-sudo ln -s /etc/sv/sddm /var/service
