0c9fdc68ad78685e1600cdc92d90b7f33460380a
hoopermangit <hooperman@hotmail.com>
2024-10-06 08:31:53 -0500

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# remover firefox
-sudo xbps-remove -Ry firefox
-sudo xbps-remove -Oov
-
 # void linux remover xfce
 
 # remover xfce
 sudo xbps-remove -Ry xfce4* xfwm4
 sudo xbps-remove -Ry libxfce4ui-4.18.6_1 xfce4-pulseaudio-plugin-0.4.8_1
 sudo xbps-remove -Oov
-sudo xbps-install -Sy elogind
 
 # reiniciar sistema
-# seleccionar lxde
 sudo reboot
