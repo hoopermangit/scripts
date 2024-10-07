7359a53bf1cb09cf43179f31fbdae333570d7529
hoopermangit <hooperman@hotmail.com>
2024-10-07 15:09:53 -0500

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -4,7 +4,6 @@
 
 # remover firefox
 sudo xbps-remove -Ry firefox
-sudo xbps-remove -Oov
 
 # actualizar repositorios
 sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree void-repo-debug
@@ -13,7 +12,7 @@ sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-no
 sudo xbps-install -Suy
 
 # instalar paquetes
-sudo xbps-install -Sy 7zip 7zip-unrar barrier barrier bpytop chromium cups curl elogind epdfview epson-inkjet-printer-escpr fastfetch filezilla flameshot galculator gparted gpicview gutenprint hunspell-es imagescan iscan kolourpaint leafpad libreoffice libreoffice-i18n-es lxde mpv nano qbittorrent rsync smplayer wget
+sudo xbps-install -Sy 7zip 7zip-unrar barrier barrier bpytop chromium cups curl elogind epdfview epson-inkjet-printer-escpr fastfetch filezilla flameshot galculator gparted gutenprint hunspell-es imagescan iscan kolourpaint leafpad libreoffice libreoffice-i18n-es lxde mpv nano qbittorrent rsync smplayer wget
 
 # instalar fonts
 sudo cp -r Menlo.Font/ Windows.Fonts/ JetBrainsMono.Font/ /usr/share/fonts/
@@ -26,10 +25,4 @@ sudo cp -r Bodhi/ Magna/ /usr/share/lxde/wallpapers/
 sudo usermod -aG lpadmin hooperman
 
 # iniciar servicios
-sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/NetworkManager /var/service
-sudo ln -s /etc/sv/lightdm /var/service
 sudo ln -s /etc/sv/cupsd /var/service
-
-# reiniciar sistema
-sudo reboot
