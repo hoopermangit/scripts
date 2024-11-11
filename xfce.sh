0aa2c4e3ea0b8b1cacff24cb6de759465520cdf6
hoopermangit <hooperman@hotmail.com>
2024-11-11 08:23:30 -0500

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -12,7 +12,7 @@ sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-no
 sudo xbps-install -Suy
 
 # instalar paquetes
-sudo xbps-install -Sy 7zip 7zip-unrar barrier barrier bpytop chromium cups curl epdfview epson-inkjet-printer-escpr fastfetch filezilla flameshot galculator gparted hunspell-es imagescan iscan kolourpaint libreoffice libreoffice-i18n-es mpv nano qbittorrent rsync smplayer wget
+sudo xbps-install -Sy 7zip 7zip-unrar barrier barrier bpytop chromium cups curl epson-inkjet-printer-escpr fastfetch filezilla flameshot galculator gparted gwenview hunspell-es imagescan iscan kolourpaint libreoffice libreoffice-i18n-es mpv nano okular qbittorrent rsync smplayer wget
 
 # instalar fonts
 sudo cp -r Menlo.Font/ Windows.Fonts/ JetBrainsMono.Font/ /usr/share/fonts/
