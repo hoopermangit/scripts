796491628489e3808a8add26bc32894ee54c59d0
hoopermangit <hooperman@hotmail.com>
2025-04-26 20:59:17 +0000

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -6,13 +6,13 @@
 sudo xbps-remove -Ry firefox
 
 # actualizar repositorios
-sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree void-repo-debug
+sudo xbps-install -Sy void-repo-debug void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 
 # actualizar sistema
 sudo xbps-install -Suy
 
 # instalar paquetes
-sudo xbps-install -Sy ark barrier barrier-gui bpytop chromium cups curl exfatprogs fastfetch ffmpegthumbs filezilla galculator gimp gparted gssmartcontrol gutenprint gwenview hunspell-es imagescan iscan kde5 kde5-baseapps kdenlive kolourpaint krita kwrite libreoffice libreoffice-i18n-es mkvtoolnix-gui mpv nano okular qbittorrent rsync smplayer spectacle unrar wget wine wine-32bit xournal
+sudo xbps-install -Sy ark bpytop chromium cups curl epson-inkjet-printer-escpr exfatprogs fastfetch ffmpegthumbs filezilla galculator gimp gparted gssmartcontrol gutenprint gwenview hblock hunspell-es iscan kde5 kde5-baseapps kdenlive kolourpaint krita kwrite libreoffice libreoffice-i18n-es mkvtoolnix-gui mpv nano okular qbittorrent rsync smplayer spectacle topgrade unrar wget wine wine-32bit xournal
 
 # instalar playonlinux
 sudo xbps-install -Sy playonlinux
