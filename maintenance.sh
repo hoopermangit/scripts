e346776f7c9581c799ccb8fe08ffa86a117ca728
hoopermangit <hooperman@hotmail.com>
2025-12-17 03:17:02 +0000

    Update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -6,9 +6,11 @@ topgrade
 # update blockers
 sudo hblock
 
-# clean cache and remove orphan files
+# clean cache
 sudo rm -rf /home/hooperman/.cache/thumbnails/large/
 sudo rm -rf /home/hooperman/.cache/thumbnails/normal/
 sudo rm -rf /home/hooperman/.cache/thumbnails/x-large/
 sudo rm -rf /home/hooperman/.cache/thumbnails/xx-large/
+
+# remove orphaned files
 sudo xbps-remove -Oov
